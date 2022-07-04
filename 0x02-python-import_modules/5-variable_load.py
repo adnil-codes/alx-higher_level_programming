d6b5049e15fcbd53c066091ebe023e7d96b3b3a8
adnil-codes <lindachukwu961@gmail.com>
2022-07-04 16:38:15 +0100

    updates

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,5 @@
 #!/usr/bin/pythom3
-from __name__ == "__main__":
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
     from varable_load_5 import a
     print(a)
